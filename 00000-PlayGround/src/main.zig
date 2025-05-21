fbd5464f1f3e3be5d78adb663c14badb9de4b56f
Ahmed Lilah <sabrylilah2325667@gmail.com>
2025-05-21 12:15:39 +0300

    Auto-commit: update in submodule Zig on 2025-05-21 12:15:32

--- 00000-PlayGround/src/main.zig
+++ 00000-PlayGround/src/main.zig
@@ -1,7 +1,39 @@
 const std = @import("std");
 
+fn isNumeric(comptime T: type) bool {
+    return switch (@typeInfo(T)) {
+        .i32, .f32, .int, .float, .comptime_int, .comptime_float => true,
+        else => false,
+    };
+}
+
+fn genericAdd(comptime T: type, A: T, B: T) !T {
+    const allocator = std.heap.page_allocator;
+
+    // if (isNumeric(T)) {
+    if (T == i32) {
+        return A + B;
+    } else if (T == []const u8) {
+        const result = try std.fmt.allocPrint(allocator, "{}{}", .{ A, B });
+        return result;
+    } else if (T == std.ArrayList(u8)) {
+        var result = std.ArrayList(u8).init(allocator);
+        try result.appendSlice(A.items);
+        try result.appendSlice(B.items);
+        return result;
+    } else {
+        @compileLog(T);
+        @compileError("Unsupported type passed to genericAdd");
+    }
+}
+
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, World!\n", .{});
-    try stdout.print("{s} {s}\n", .{ "I'm", "here." });
+
+    try stdout.print("5 + 5 = {}\n", .{try genericAdd(i32, 5, 5)});
+    try stdout.print("5.0 + 5.0 = {}\n", .{try genericAdd(f32, 5.0, 5.0)});
+
+    const first = "Ahmed ";
+    const second = "Sabry.";
+    try stdout.print("Full name: {s}\n", .{try genericAdd([]const u8, first, second)});
 }
